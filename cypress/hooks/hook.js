c0e5b3294f10580da1cdaf8c6abead8a6d2e6416
DhonyImamSaputra <dhony.imamsaputra@larisin.id>
2020-08-17 23:27:19 +0700

    add file for hook reporting

---
+++ cypress/hooks/hook.js
@@ -0,0 +1,27 @@
+const report = require('multiple-cucumber-html-reporter');
+
+report.generate({
+	jsonDir: '../report/cucumber-json',
+	reportPath: '../report/html',
+	metadata:{
+        browser: {
+            name: 'chrome',
+            version: '60'
+        },
+        device: 'Local test machine',
+        platform: {
+            name: 'mac os x',
+            version: '10.15.6'
+        }
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            {label: 'Project', value: 'Custom project'},
+            {label: 'Release', value: '1.2.3'},
+            {label: 'Cycle', value: 'B11221.34321'},
+            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+        ]
+    }
+});
